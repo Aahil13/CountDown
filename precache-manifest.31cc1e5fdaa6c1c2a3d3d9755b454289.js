07aac1c868b15995a19a19c5557c46798014f0a7
onyeanuna prince eze <onyeanunaprince@gmail.com>
2022-01-20 00:45:03 +0100

    Updates

---
+++ precache-manifest.31cc1e5fdaa6c1c2a3d3d9755b454289.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bea5e33ebffaa541f2df95f6ba56be9",
+    "url": "/CountDown/index.html"
+  },
+  {
+    "revision": "708bb94039a6de48f950",
+    "url": "/CountDown/static/js/2.f7a1073b.chunk.js"
+  },
+  {
+    "revision": "6519c7eb8d4de30f8f73",
+    "url": "/CountDown/static/js/main.9dc09f78.chunk.js"
+  },
+  {
+    "revision": "fcd61b744913c71828fa",
+    "url": "/CountDown/static/js/runtime-main.947e005f.js"
+  },
+  {
+    "revision": "3661648f676dc75115eca2119880f203",
+    "url": "/CountDown/static/media/bg-stars.3661648f.svg"
+  },
+  {
+    "revision": "33fc1888e35b120a24a9da8cce94539f",
+    "url": "/CountDown/static/media/icon-facebook.33fc1888.svg"
+  },
+  {
+    "revision": "40c2e0b39fdcae64ec665da1ada69758",
+    "url": "/CountDown/static/media/icon-instagram.40c2e0b3.svg"
+  },
+  {
+    "revision": "67ee8378ca671d05bc5e3f2d3f5af096",
+    "url": "/CountDown/static/media/icon-pinterest.67ee8378.svg"
+  },
+  {
+    "revision": "8fad4803a614a1d56fd923b5f124f3a0",
+    "url": "/CountDown/static/media/pattern-hills.8fad4803.svg"
+  }
+]);
